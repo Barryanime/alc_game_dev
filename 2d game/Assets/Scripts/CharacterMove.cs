e5332ad61aca42cb3910f84b7e4b07ac836c79d5
Unknown <Barryanime123@gmail.com>
2018-09-11 10:31:09 -0600

    Jump

--- 2d game/Assets/Scripts/CharacterMove.cs
+++ 2d game/Assets/Scripts/CharacterMove.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int moveSpeed = 10;
-
+	public float JumpHeight;
+	
+	//Player Grounded Variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+	
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+			Jump();
+		}
+
+		// This codee makes the character move from side to sdie using the A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>
+		}
+	}
+	
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
