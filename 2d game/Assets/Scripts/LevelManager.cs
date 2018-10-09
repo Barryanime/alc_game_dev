46fe42592a2ef36b0a972824bb46e5c739298a4d
Unknown <Barryanime123@gmail.com>
2018-10-09 10:26:58 -0600

    Respawn player

--- 2d game/Assets/Scripts/LevelManager.cs
+++ 2d game/Assets/Scripts/LevelManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour {
 
 	public GameObject CurrentCheckPoint;
-	private Rigidbody2D PC;
+	public Rigidbody2D PC;
 
 	//Particles
 	public GameObject DeathParticle;
@@ -28,7 +28,7 @@ public class LevelManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		PC = FindObjectOfType<Rigidbody2D> ();
+		// PC = FindObjectOfType<Rigidbody2D> ();
 	}
 
 	public void RespawnPlayer(){
